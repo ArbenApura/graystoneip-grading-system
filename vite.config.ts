be696f3578ed84d3ec7ca538db7c6040e73e9db0
Arben Apura <arbenapura.official@gmail.com>
2023-06-22 22:05:16 +0800

    Changed into a static adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
+import * as path from 'path';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const config: UserConfig = {
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$styles: path.resolve('./src/styles'),
+			$components: path.resolve('./src/components'),
+			$utils: path.resolve('./src/utils'),
+			$stores: path.resolve('./src/stores'),
+			$assets: path.resolve('./src/assets')
+		}
+	}
+};
+
+export default config;
