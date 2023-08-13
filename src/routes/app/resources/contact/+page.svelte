a247222e13ee96c22b42f91ab8cb0d137c7fb805
Arben Apura <arbenapura.official@gmail.com>
2023-08-13 15:20:38 +0800

    Added /app/resources/*

---
+++ src/routes/app/resources/contact/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	// IMPORTED COMPONENTS
+	import Header from '$components/layouts/Header';
+</script>
+
+<Header
+	breadcrumbItems={[
+		{ icon: 'ph-bold ph-folders', label: 'Resources', href: '' },
+		{ label: 'Contact', href: '/app/resources/contact' },
+	]}
+/>
